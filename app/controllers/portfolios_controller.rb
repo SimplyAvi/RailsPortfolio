59989915a300e9f8285ec6c7176d295d4c1c7cca
SimplyAvi <a.totaram@gmail.com>
2020-01-21 05:28:43 -0500

    integrated new and create actions for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,7 +5,30 @@ class PortfoliosController < ApplicationController
         @portfolio_items = Portfolio.all
     end
 
+    def new
+        @portfolio_item = Portfolio.new
+    end
     
+
+    def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your Portfolio item is now live!' }
+        # format.json { render :show, status: :created, location: @portfolio_item }
+      else
+        format.html { render :new }
+        # format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+     end
+    end
+
+
+
+
+
+
 
 
 
