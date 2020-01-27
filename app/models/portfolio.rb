4629e63eb3e13dcf468f781e296e73d335e98672
SimplyAvi <a.totaram@gmail.com>
2020-01-27 05:40:34 -0500

    implimented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -5,4 +5,12 @@ class Portfolio < ApplicationRecord
         where(subtitle: 'Angular')
     end
     scope :ruby_on_rails_portfolio_items, -> {where(subtitle:'Ruby on Rails')}
+
+    after_initialize :set_defaults 
+
+    def set_defaults
+        self.main_image ||= "https://place-hold.it/600x400"
+        self.thumb_image ||= "https://place-hold.it/350x200"
+    end
+
 end
