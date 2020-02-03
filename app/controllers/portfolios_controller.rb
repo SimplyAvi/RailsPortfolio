a2e4d900f6e2a22aef1354e2922991e83444308a
SimplyAvi <a.totaram@gmail.com>
2020-02-03 12:19:37 -0500

    Updated portfolio to include attribute for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,7 +7,7 @@ class PortfoliosController < ApplicationController
     def index 
         # @portfolio_items = Portfolio.ruby_on_rails_portfolio_items
         # @portfolio_items = Portfolio.angular
-        @portfolio_items = Portfolio.all
+        @portfolio_items = Portfolio.by_position
     end
 
     def angular 
