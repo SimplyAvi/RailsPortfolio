595886e3629bf24e39716fe2fa42c61f8779a4bc
SimplyAvi <a.totaram@gmail.com>
2020-02-13 15:00:32 -0500

    updated blog shared form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -19,6 +19,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs/new
   def new
+    
     @blog = Blog.new
   end
 
@@ -34,10 +35,10 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
+     
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+       
       end
     end
   end
