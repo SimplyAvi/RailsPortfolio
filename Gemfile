3c2b64f18344972f918aa7334ba4310541203101
SimplyAvi <a.totaram@gmail.com>
2020-02-19 13:58:14 -0500

    Implimented initial connection to twitter API

--- Gemfile
+++ Gemfile
@@ -96,3 +96,5 @@ gem 'dotenv-rails', '~> 2.7', '>= 2.7.5'
 gem 'cocoon', '~> 1.2', '>= 1.2.14'
 
 gem 'gritter', '~> 1.2'
+
+gem 'twitter', '~> 7.0'
---
+++ lib/social_tool.rb
@@ -0,0 +1,20 @@
+module SocialTool
+    def self.twitter_search
+        client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV.fetch("twitter_consumer_key")
+        config.consumer_secret     = ENV.fetch("twitter_cosnsumer_secret")
+        config.access_token        = ENV.fetch("twitter_access_token")
+        config.access_token_secret = ENV.fetch("twitter_access_token_secret")
+        end
+
+        client.search("#rails", result_type: 'recent').take(6).collect do |tweet|
+            "{tweet.user.screen_name}: #{tweet.text}"
+        end
+
+        # client.search("to:justinbieber marry me", result_type: "recent").take(3).collect do |tweet|
+        #  "#{tweet.user.screen_name}: #{tweet.text}"
+        # end
+
+    end
+    
+end
