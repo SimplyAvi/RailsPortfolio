a58971c3db4e34689c2696f8bd7a009fc0818f7f
SimplyAvi <a.totaram@gmail.com>
2020-02-20 16:05:20 -0500

    Implimented action cable for comments

--- app/jobs/comment_broadcast_job.rb
+++ app/jobs/comment_broadcast_job.rb
@@ -3,12 +3,14 @@ class CommentBroadcastJob < ApplicationJob
 
     def perform(comment)
         ActionCable.server.broadcast "blogs_#{comment.blog.id}_channel", comment: render_comment(comment)
+
     end
 
     private
 
     def render_comment(comment)
-        CommentsController.render partial: "comments/comment", locals: {comment: comment}
+        CommentsController.render partial: 'comments/comment', locals: { comment: comment }
+
     end
 
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
 
-  validates :content, presence: true, length: {minimum: 5, maximum:1000}
+  validates :content, presence: true, length: {minimum: 5, maximum: 1000}
+
 
   after_create_commit {CommentBroadcastJob.perform_later(self) }
+  
 end
