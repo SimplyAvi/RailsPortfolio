bc09b45fc8d50cbbd00cff6e22a2965844955893
SimplyAvi <a.totaram@gmail.com>
2020-03-03 12:53:58 -0500

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+   config.action_cable.allowed_request_origins = ['https://simplyavi.herokuapp.com/', 'http://simplyavi.herokuapp.com/']
+  config.action_cable.url = "wss://simplyavi.herokuapp.com/cable"
 end
