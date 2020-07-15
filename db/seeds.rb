1b0fba1961d462ffd2da9340e4f37e979586c915
SimplyAvi <a.totaram@gmail.com>
2020-07-15 08:20:45 -0400

    updated the seeds file again to have a more professional feeling

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ puts('created 3 topics')
 
 
 
-10.times do |blog|
+3.times do |blog|
     Blog.create!(
         title: "My Blog Post #{blog}",
         body: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?",
@@ -37,7 +37,7 @@ puts('created 3 topics')
     )
 end
 
-puts('10 blogs created')
+puts('3 blogs created')
 # byebug
 
 
@@ -65,7 +65,7 @@ puts('10 blogs created')
 
 puts('5 skills created')
 
-8.times do |portfolio|
+3.times do |portfolio|
     Portfolio.create!(
         title: "Portfolio title: #{portfolio}" ,
         subtitle: "Ruby on Rails" ,
@@ -93,4 +93,4 @@ puts('9 portfolio created')
         )
 end
 
-puts('3 echnologies created')
+puts('3 technologies created')
